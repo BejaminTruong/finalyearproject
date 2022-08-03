9e831c78f7255fb93e5397fcc875ecde9e89b74e
truong thai binh <thaibinh291201@gmail.com>
2022-08-04 06:11:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="trelloContainer">
+      <nav className='navbar app'>App bar</nav>
+      <nav className='navbar board'>Board bar</nav>
+      <div className="boardColumns">
+        Board column
+      </div>
+    </div>
+  );
+}
+
+export default App;
