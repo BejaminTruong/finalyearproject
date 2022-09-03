0a6578d79b1ad289de045d4014e3a783dcbf4ab8
thai binh <thaibinh291201@gmail.com>
2022-09-03 16:48:46 +0700

    design trello

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,264 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="trelloContainer">
-      <nav className='navbar app'>App bar</nav>
-      <nav className='navbar board'>Board bar</nav>
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
       <div className="boardColumns">
-        Board column
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://picsum.photos/200/400"
+                alt="benjamintruong_alt"
+              />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200" alt="benjamintruong_alt" />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://picsum.photos/200/400"
+                alt="benjamintruong_alt"
+              />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200" alt="benjamintruong_alt" />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://picsum.photos/200/400"
+                alt="benjamintruong_alt"
+              />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200" alt="benjamintruong_alt" />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://picsum.photos/200/400"
+                alt="benjamintruong_alt"
+              />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200" alt="benjamintruong_alt" />
+              Title: Benjamin Truong
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              voluptatum.
+            </li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem,
+              minima.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              quia!
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+              fuga.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              culpa.
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
       </div>
     </div>
   );
